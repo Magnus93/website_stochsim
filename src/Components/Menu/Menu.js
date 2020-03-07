c91689b8d96fbf3d0142fe10298061e4802cb256
StochSD Team <nope>
2020-03-07 14:39:29 +0100

    Add submenu

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -9,10 +9,17 @@ const menu = () => {
     <div className="menu">
       <img src={Logo} alt="Icon is missing" id="iconImg" />
 			<h1>StochSD</h1>
-      <p><Link to="/home">Purpose of Stochastic Simulation - StochSD</Link></p>
-      <p><Link to="/stochsd_software">StochSD Software</Link> (Download)</p>
-      <p><Link to="/stochsd_and_statres_manuels">StochSD User's Manuals</Link></p>
-      <p><Link to="/example_models">Example Models</Link></p>
+      <ul>
+        <li><Link to="/home">Purpose of Stochastic Simulation - StochSD</Link></li>
+        <li><Link to="/stochsd_software">StochSD Software</Link>
+        <ul>
+          <li><a href="https://sourceforge.net/projects/stochsd/files/" target="_blank">Download</a></li>
+          <li><a href="https://stochsd.sourceforge.io/software/" target="_blank">Try online</a></li>
+        </ul>
+        </li>
+        <li><Link to="/stochsd_and_statres_manuels">StochSD User's Manuals</Link></li>
+        <li><Link to="/example_models">Example Models</Link></li>
+      </ul>
 			<img src={Bath} alt={"Bath here"} id="bathImg" />
     </div>
   );
