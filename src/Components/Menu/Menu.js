4efc8bb51efdb8759341f96fed6518cefb757872
StochSD Team <nope>
2020-03-07 15:25:10 +0100

    Update menu

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -10,15 +10,16 @@ const menu = () => {
       <img src={Logo} alt="Icon is missing" id="iconImg" />
 			<h1>StochSD</h1>
       <ul>
-        <li><Link to="/home">Purpose of Stochastic Simulation - StochSD</Link></li>
-        <li><Link to="/stochsd_software">StochSD Software</Link>
+        <li className="topmenu"><Link to="/home">Purpose of Stochastic Simulation</Link></li>
+        <li className="topmenu"><Link to="/stochsd_software">StochSD Software</Link>
         <ul>
-          <li><a href="https://sourceforge.net/projects/stochsd/files/" target="_blank">Download</a></li>
-          <li><a href="https://stochsd.sourceforge.io/software/" target="_blank">Try online</a></li>
+          <li><a className="submenu" href="https://sourceforge.net/projects/stochsd/files/" target="_blank">Download StochSD</a></li>
+          <li><a className="submenu" href="https://github.com/stochsd/stochsd/" target="_blank">Download Source code</a></li>
+          <li><a className="submenu" href="https://stochsd.sourceforge.io/software/" target="_blank">Try online</a></li>
         </ul>
         </li>
-        <li><Link to="/stochsd_and_statres_manuels">StochSD User's Manuals</Link></li>
-        <li><Link to="/example_models">Example Models</Link></li>
+        <li className="topmenu"><Link to="/stochsd_and_statres_manuels">StochSD User's Manuals</Link></li>
+        <li className="topmenu"><Link to="/example_models">Example Models</Link></li>
       </ul>
 			<img src={Bath} alt={"Bath here"} id="bathImg" />
     </div>
